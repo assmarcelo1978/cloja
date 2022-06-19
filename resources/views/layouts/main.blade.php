eeec16e99d73848391a3e4fac56db185071418af
Marcelo Assunção <assmarcelo1978@gmail.com>
2022-06-19 09:55:50 -0300

    xxx

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,9 +9,9 @@
         <!-- Styles -->
         <link href                = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="/css/styles.css">
-                
+
   </head>
-          
+
           {{-- Yeld body --}}
           @yield('content')
           <!-- Scripts -->
@@ -20,7 +20,7 @@
           <script nomodule src    = "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
           <!-- Bootstrap -->
         <script src               = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-      
+
         <footer class="footer sticky fixed-bottom text-center">
           <p>@yield('title') - &copy 2022</p>
         </footer>
